9d671e84c15aebb3912a867bee0e3e60f1d6c314
Sachi Nagada <sachi.nagada@vimeo.com>
2020-03-05 16:07:18 -0500

    add tag copying to toml and tests

--- toml/toml.go
+++ toml/toml.go
@@ -1,12 +1,20 @@
 package toml
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"reflect"
 
-	tomlparser "github.com/pelletier/go-toml"
 	"github.com/vimeo/dials"
+	"github.com/vimeo/dials/tagformat"
+	"github.com/vimeo/dials/transform"
+
+	tomlparser "github.com/pelletier/go-toml"
 )
+
+// TOMLTagName is the name of the `"toml"` tag.
+const TOMLTagName = "toml"
 
 // Decoder is a decoder than understands TOML.
 type Decoder struct {
@@ -15,13 +23,31 @@ type Decoder struct {
 // Decode will read from `r` and parse it as TOML depositing the relevant values
 // in `t`.
 func (d *Decoder) Decode(r io.Reader, t *dials.Type) (reflect.Value, error) {
-	decoder := tomlparser.NewDecoder(r)
+	tomlBytes, err := ioutil.ReadAll(r)
+	if err != nil {
+		return reflect.Value{}, fmt.Errorf("error reading TOML: %s", err)
+	}
 
-	instance := reflect.New(t.Type()).Interface()
-	err := decoder.Decode(instance)
+	// If there aren't any toml tags, copy over from any dials tags.
+	tfmr := transform.NewTransformer(t.Type(),
+		&tagformat.TagCopyingMangler{
+			SrcTag: tagformat.DialsTagName, NewTag: TOMLTagName})
+	val, tfmErr := tfmr.Translate()
+	if tfmErr != nil {
+		return reflect.Value{}, fmt.Errorf("failed to convert tags: %s", tfmErr)
+	}
+
+	// Get a pointer to our value, so we can pass that.
+	instance := val.Addr().Interface()
+	err = tomlparser.Unmarshal(tomlBytes, instance)
 	if err != nil {
 		return reflect.Value{}, err
 	}
 
-	return reflect.ValueOf(instance), nil
+	unmangledVal, unmangleErr := tfmr.ReverseTranslate(val)
+	if unmangleErr != nil {
+		return reflect.Value{}, unmangleErr
+	}
+
+	return unmangledVal, nil
 }
