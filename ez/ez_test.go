d2bc0be63029b0128993b0c4e16490ec53419b4f
David Finkel <davidf@vimeo.com>
2020-09-08 16:49:09 -0400

    ez: add tests for verified config/watching

--- ez/ez_test.go
+++ ez/ez_test.go
@@ -2,14 +2,17 @@ package ez
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-type Config struct {
+type config struct {
 	// Path will contain the path to the config file and will be set by
 	// environment variable
 	Path string              `dials:"CONFIGPATH"`
@@ -20,13 +23,13 @@ type Config struct {
 
 // ConfigPath reflects where the path to config file is stored. Path field
 // will be populated from environment variable
-func (c *Config) ConfigPath() (string, bool) {
+func (c *config) ConfigPath() (string, bool) {
 	return c.Path, true
 }
 
 // TestYAMLConfigEnvFlag cannot run concurrently with other tests because of
 // environment manipulation.
-func TestYAMLConfigEnvFlag(t *testing.T) {
+func TestYAMLConfigEnvFlagWithValidConfig(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -34,12 +37,12 @@ func TestYAMLConfigEnvFlag(t *testing.T) {
 	require.NoError(t, envErr)
 	defer os.Unsetenv("CONFIGPATH")
 
-	c := &Config{}
+	c := &config{}
 	view, dialsErr := YAMLConfigEnvFlag(ctx, c)
 	require.NoError(t, dialsErr)
 
 	// Val1 and Val2 come from the config file and Path will be populated from env variable
-	expectedConfig := Config{
+	expectedConfig := config{
 		Path: "../testhelper/testconfig.yaml",
 		Val1: 456,
 		Val2: "hello-world",
@@ -49,6 +52,89 @@ func TestYAMLConfigEnvFlag(t *testing.T) {
 			"Jack":  {},
 		},
 	}
-	populatedConf := view.View().(*Config)
+	populatedConf := view.View().(*config)
 	assert.EqualValues(t, expectedConfig, *populatedConf)
+}
+
+type validatingConfig struct {
+	// Path will contain the path to the config file and will be set by
+	// environment variable
+	Path string              `dials:"CONFIGPATHFIM"`
+	Val1 int                 `dials:"Val1"`
+	Val2 string              `dials:"Val2"`
+	Set  map[string]struct{} `dials:"Set"`
+}
+
+// ConfigPath reflects where the path to config file is stored. Path field
+// will be populated from environment variable
+func (c *validatingConfig) ConfigPath() (string, bool) {
+	return c.Path, true
+}
+
+func (c *validatingConfig) Verify() error {
+	if c.Val1 > 200 {
+		return fmt.Errorf("val1 %d > 200", c.Val1)
+	}
+	return nil
+}
+
+func TestYAMLConfigEnvFlagWithValidatingConfig(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	tmpFile, tmpErr := ioutil.TempFile("", "")
+	require.NoError(t, tmpErr)
+	tmpFile.Write([]byte("Val1: 789"))
+	require.NoError(t, tmpFile.Sync())
+	require.NoError(t, tmpFile.Close())
+	path := tmpFile.Name()
+	defer os.Remove(path)
+
+	c := &validatingConfig{Path: path}
+	view, dialsErr := YAMLConfigEnvFlag(ctx, c)
+	assert.NotNil(t, view)
+	require.EqualError(t, dialsErr, "failed to stack/verify config with file layered: val1 789 > 200")
+}
+
+func TestYAMLConfigEnvFlagWithValidatingConfigInitiallyValid(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	tmpDir, tmpErr := ioutil.TempDir("", "")
+	require.NoError(t, tmpErr)
+	defer os.RemoveAll(tmpDir)
+	path := filepath.Join(tmpDir, "fim1.yaml")
+	require.NoError(t, ioutil.WriteFile(path, []byte("Val1: 189"), 0660))
+	defer os.Remove(path)
+
+	errCh := make(chan error)
+	errHandler := func(ctx context.Context, err error, oldConfig, newConfig interface{}) {
+		errCh <- err
+	}
+	c := &validatingConfig{Path: path}
+	view, dialsErr := YAMLConfigEnvFlag(ctx, c, WithOnWatchedError(errHandler), WithWatchingConfigFile(true))
+	require.NoError(t, dialsErr)
+	assert.NotNil(t, view)
+
+	expectedConfig := validatingConfig{
+		Path: path,
+		Val1: 189,
+		Val2: "",
+		Set:  map[string]struct{}{},
+	}
+	populatedConf := view.View().(*validatingConfig)
+	assert.EqualValues(t, expectedConfig, *populatedConf)
+
+	tmpPath2 := filepath.Join(tmpDir, "_tmp_fim1.yaml")
+	require.NoError(t, ioutil.WriteFile(tmpPath2, []byte("Val1: 201"), os.FileMode(0660)))
+
+	require.NoError(t, os.Rename(tmpPath2, path))
+
+	// Write a new version of the config
+	select {
+	case newcfg := <-view.Events():
+		t.Errorf("unexpected new version; should have failed verification: %+v", newcfg)
+	case err := <-errCh:
+		require.EqualError(t, err, "val1 201 > 200")
+	}
 }
