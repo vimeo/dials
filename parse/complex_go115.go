5a29cfbcc8588e176882aa93125deb01c4d70776
Sergio Salvatore <sergio@vimeo.com>
2020-12-22 14:50:23 -0500

    Complex Parsing
    Since go >= 1.15 has strconv.ParseComplex, let's delegate to that where
    available.  strconv is also a bit stricter on the format of the complex
    number string, so adjust tests to reflect that.  Since few (if any)
    people are using this, it shouldn't matter much.

---
+++ parse/complex_go115.go
@@ -0,0 +1,18 @@
+// +build go1.15
+
+package parse
+
+import (
+	"strconv"
+)
+
+// Complex64 delegates to strconv.ParseComplex for go >= 1.15.
+func Complex64(s string) (complex64, error) {
+	c, err := strconv.ParseComplex(s, 64)
+	return complex64(c), err
+}
+
+// Complex128 delegates to strconv.ParseComplex for go >= 1.15.
+func Complex128(s string) (complex128, error) {
+	return strconv.ParseComplex(s, 128)
+}
